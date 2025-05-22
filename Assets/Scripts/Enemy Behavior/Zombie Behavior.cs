91286424725345e576e9a22ef0d32d7788d67218
blueys123232 <134318214+blueys123232@users.noreply.github.com>
2025-05-22 06:55:30 +0100

    New Levels Updated
    There are so many changes for my levels
    
    Now there are acids in level 11 and 12
    
    Level 13 design is almost finished
    
    I am Finished Working my Zombie Script

--- Assets/Scripts/Enemy Behavior/Zombie Behavior.cs
+++ Assets/Scripts/Enemy Behavior/Zombie Behavior.cs
@@ -11,7 +11,7 @@ public class ZombieBehavior : MonoBehaviour
     private Animator animator;
     private Rigidbody2D rb;
     private Transform player;
- 
+
 
     private bool isAttacking = false;
 
@@ -22,14 +22,62 @@ public class ZombieBehavior : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-        player = GameObject.FindWithTag("Player").transform;
-        ppScript = GetComponent <PatrolPointScript>();
-        flipE = GetComponent<FlipEnemy>();
+        player = GameObject.FindWithTag("Player").transform; // assmuning the player is tagged "Player"
+        ppScript = GetComponent<PatrolPointScript>(); //GetThePatrolPointScript
+        flipE = GetComponent<FlipEnemy>(); // Get The FlipEnemyScript
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float distanceToPlayer = Vector2.Distance(transform.position, player.position);
+
+        if (distanceToPlayer <= detectionRange)
+            if (distanceToPlayer <= detectionRange)
+            {
+                if (distanceToPlayer <= attackRange)
+                {
+                    AttackPlayer();
+                }
+                // OtherWise Follow the Player
+                else
+                {
+                    FollowPlayer();
+                }
+            }
+            else
+            {
+                ppScript.Patrol();
+            }
+
+    }
+    void FollowPlayer()
+    {
+        Vector2 moveDirection = (player.position - transform.position).normalized;
+        rb.velocity = new Vector2(moveDirection.x * speed, rb.velocity.y);
+
+        animator.SetBool("isWalking", true);
+        flipE.FlipDirection();  // Flip to face the player
+    }
+    void AttackPlayer()
+    {
+        if (!isAttacking)
+        {
+            isAttacking = true;
+            animator.SetTrigger("isAttacking");
+
+
+
+
+            StartCoroutine(ResetAttack());
+
+        }
+
+    }
+
+    private IEnumerator ResetAttack()
+    {
+        yield return new WaitForSeconds(1f);
+        isAttacking = false;
     }
 }
