6f15eecf8314b640e9c1e5d0f6907d8b8166175c
GDLinford <george.david.linford@gmail.com>
2025-07-21 11:58:06 +0100

    Work with Lukas 21_07_25

--- Assets/Scripts/WeaponScripts/AmmoPickup.cs
+++ Assets/Scripts/WeaponScripts/AmmoPickup.cs
@@ -1,17 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AmmoPickup : MonoBehaviour
 {
+
     private bool isCollected = false;
     private AudioSource audioSource;
     PickUpmanager pickupmanager;
     public int AmmoPickupAmount;
+    WeaponStats wStats;
 
     // Start is called before the first frame update
     void Start()
     {
+        wStats = FindAnyObjectByType<WeaponStats>();
         audioSource = GetComponent<AudioSource>();
         pickupmanager = FindAnyObjectByType<PickUpmanager>();
     }
@@ -32,6 +36,4 @@ public class AmmoPickup : MonoBehaviour
         yield return new WaitForSeconds(audioSource.clip.length);
         Destroy(gameObject); // Destroy the coin after the sound has played
     }
-
-
 }
---
+++ Assets/Scripts/WeaponScripts/WeaponBase.cs
@@ -0,0 +1,23 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//[CreateAssetMenu(fileName = "Weapon", menuName = "Weapon/Create new Weapon")]
+//public class WeaponBase : ScriptableObject
+//{
+//    [SerializeField] string weaponName;
+
+//    [SerializeField] Sprite WeaponSprite;
+//    [SerializeField] AmmoType atype;
+
+//    //Weapon Stats
+//    [SerializeField] int damage, ammoCount;
+//    [SerializeField] float projectileSpeed;
+//}
+
+//public enum AmmoType
+//{
+//    none,
+//    bullet,
+//    shell
+//}
